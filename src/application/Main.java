4f5f7a1213dfe53bbf956461cdbf4988e08d8144
valadao-davi <davi.raichu@gmail.com>
2025-01-09 12:24:22 -0300

    feat: reading .csv archives and separating per commas

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,35 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Set the path for your products: ");
+		String strPath =  sc.nextLine();
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(strPath))){
+			String line = br.readLine();
+			while(line != null) {
+				String[] lineString = line.toString().split(",");
+				for(String item : lineString) {
+					System.out.println(item);
+				}
+				line = br.readLine();
+			}
+		}catch(IOException e) {
+			System.out.println("An error occured in the file system:");
+		}
+		
+		sc.close();
 	}
 
 }
--- src/model/entities/Product.java
+++ src/model/entities/Product.java
@@ -1,5 +1,44 @@
 package model.entities;
 
 public class Product {
+	private String name;
+	private double unitPrice;
+	private int quantity;
+	
+	public Product(String name, double unitPrice, int quantity) {
+		this.name = name;
+		this.unitPrice = unitPrice;
+		this.quantity = quantity;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	public double calculatePrice() {
+		return this.quantity * this.unitPrice;
+	}
+	
+	
 }
